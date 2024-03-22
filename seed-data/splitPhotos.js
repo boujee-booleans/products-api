8d134cf5ecfaf05d09ab35a6d3ece1550bc1112f
Stephen Capper <stephencapper@gmail.com>
2024-03-21 18:47:04 -0700

    add splitting functions to batch/reduce size of csv files

---
+++ seed-data/splitPhotos.js
@@ -0,0 +1,52 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const csvFilePathStyles = path.join(__dirname, 'raw', 'photos.csv');
+
+const csvBatchPath = path.join(__dirname, 'raw-batches/');
+
+const styleLimits = [245021, 489000, 733439, 977197, 1222226, 1467658, 1712625, 1956949, 10000000];
+
+fs.readFile(csvFilePathStyles, 'utf8')
+  .then((data) => {
+    console.log('Got here');
+    const rows = data.split('\n');
+    console.log('Got here???');
+    let style_id = 0;
+    let headerRow = rows[0];
+    const batchedSplitData = [];
+    let splitData = [];
+    curr_style_data = '';
+    let styleLimitIndex = 0;
+    let styleLimit = styleLimits[0];
+    for (let i = 1; i < rows.length; i++) {
+      const style_id_start = rows[i].indexOf(',') + 1;
+      const style_id_end = rows[i].indexOf(',', style_id_start);
+      const curr_style_id = Number(rows[i].slice(style_id_start, style_id_end));
+
+      if (curr_style_id !== style_id) {
+        style_id = curr_style_id;
+        splitData.push(curr_style_data);
+        curr_style_data = rows[i];
+        if (splitData.length >= styleLimit) {
+          styleLimitIndex ++;
+          styleLimit = styleLimits[styleLimitIndex];
+          const nextStyle_id = rows[i].slice(0, rows[i].indexOf(','));
+          batchedSplitData.push(headerRow + splitData.join('\n'));
+          splitData = [];
+        }
+      } else {
+        curr_style_data += '\n' + rows[i];
+      }
+    }
+    splitData.push(curr_style_data);
+    const finalStyle_id = splitData[splitData.length - 1].slice(0, splitData[splitData.length - 1].indexOf(','));
+    batchedSplitData.push(headerRow + splitData.join('\n'));
+    batchedSplitData.forEach((splitData, index) => {
+      const csvBatchFilePath = `${csvBatchPath}photos-${index + 1}.csv`;
+      fs.writeFile(csvBatchFilePath, splitData)
+        .then(() => {
+          console.log('File created: ', csvBatchFilePath)
+        });
+    });
+  })
---
+++ seed-data/splitStyles.js
@@ -0,0 +1,48 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const csvFilePathStyles = path.join(__dirname, 'raw', 'styles.csv');
+
+const csvBatchPath = path.join(__dirname, 'raw-batches/');
+
+const maxProducts = 100000;
+
+fs.readFile(csvFilePathStyles, 'utf8')
+  .then((data) => {
+    const rows = data.split('\n');
+    let product_id = 0;
+    let headerRow = rows[0];
+    const batchedSplitData = [];
+    let splitData = [];
+    curr_prod_data = '';
+    for (let i = 1; i < rows.length; i++) {
+      const product_id_start = rows[i].indexOf(',') + 1;
+      const product_id_end = rows[i].indexOf(',', product_id_start);
+      const curr_product_id = Number(rows[i].slice(product_id_start, product_id_end));
+
+      if (curr_product_id !== product_id) {
+        product_id = curr_product_id;
+        splitData.push(curr_prod_data);
+        curr_prod_data = rows[i];
+        if (splitData.length === 100000) {
+          const nextStyle_id = rows[i].slice(0, rows[i].indexOf(','));
+          batchedSplitData.push(headerRow + splitData.join('\n'));
+          console.log('File ', batchedSplitData.length, ' next style_id is: ', nextStyle_id);
+          splitData = [];
+        }
+      } else {
+        curr_prod_data += '\n' + rows[i];
+      }
+    }
+    splitData.push(curr_prod_data);
+    const finalStyle_id = splitData[splitData.length - 1].slice(0, splitData[splitData.length - 1].indexOf(','));
+    batchedSplitData.push(headerRow + splitData.join('\n'));
+    console.log('File ', batchedSplitData.length, ' final style_id is: ', finalStyle_id);
+    batchedSplitData.forEach((splitData, index) => {
+      const csvBatchFilePath = `${csvBatchPath}styles-${index + 1}.csv`;
+      fs.writeFile(csvBatchFilePath, splitData)
+        .then(() => {
+          console.log('File created: ', csvBatchFilePath)
+        });
+    });
+  })
