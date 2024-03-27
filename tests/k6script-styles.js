1538c7ee9aa4458a0901e3ba0300cbeba982e6aa
Stephen Capper <stephencapper@gmail.com>
2024-03-26 17:10:19 -0700

    update K6 test for styles

--- tests/k6script-styles.js
+++ tests/k6script-styles.js
@@ -1,7 +1,23 @@
 import http from 'k6/http';
 
+// export default function () {
+//   for (let id=1; id <= 5; id += 1) {
+//     http.get(`http://localhost:3000/products/${id}/styles`);
+//   }
+// }
+
+export const options = {
+  scenarios: {
+    open_model: {
+      executor: 'constant-arrival-rate',
+      rate: 100,
+      timeUnit: '1s',
+      duration: '30s',
+      preAllocatedVUs: 1,
+    },
+  },
+};
+
 export default function () {
-  for (let id=1; id <= 1000000; id += 99988) {
-    http.get(`http://localhost:3000/products/${id}/styles`);
-  }
+  http.get(`http://localhost:3000/products/${Math.ceil(Math.random() * 9.99 + 0.01)}/styles`);
 }
